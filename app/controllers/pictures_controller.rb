38e072c9a0673774b9bb0a7f94e4532a0083b735
Murugan <murugu2012@gmail.com>
2017-05-31 13:27:48 +0530

    Complete the CRUD resource

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,29 +1,46 @@
 class PicturesController < ApplicationController
+  
   def new_form
+    @photo = Photo.new
     render("pic_templates/new_form.html.erb")
   end
 
   def create_row
-    render("pic_templates/create_row.html.erb")
+    @photo = Photo.new(photo_params)
+    @photo.save
+    redirect_to action: 'show', id: @photo.id
   end
 
   def index
+    @photos = Photo.all
     render("pic_templates/index.html.erb")
   end
 
   def show
+    @photo = Photo.find(params[:id])
     render("pic_templates/show.html.erb")
   end
 
   def edit_form
+    @photo = Photo.find(params[:id])
     render("pic_templates/edit_form.html.erb")
   end
 
   def update_row
-    render("pic_templates/update_row.html.erb")
+    @photo = Photo.find(params[:id])
+    @photo.update_attributes(photo_params)
+    redirect_to action: 'show', id: @photo.id
   end
 
   def destroy_row
-    render("pic_templates/destroy_row.html.erb")
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_path
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:source, :caption)
   end
 end
